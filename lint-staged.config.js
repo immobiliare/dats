8d65f365d29400a0e92fb3a4e4a9c2bb48e7c720
Daniele Belardi <dnlup.dev@gmail.com>
2021-10-29 10:32:40 +0200

    build: refactyor lint-staged config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,6 +2,9 @@ module.exports = {
     '*.{js,css,json,md,yaml,yml}': ['prettier --write'],
     '*.md': (filenames) =>
         filenames.map((filename) => `'markdown-toc -i ${filename}`),
-    '*.ts': ['npm run style:lint', 'npm run style:prettier'],
-    '*.ts': () => 'tsc -p tsconfig.json --noEmit',
+    '*.ts': [
+        'npm run style:lint',
+        'npm run style:prettier',
+        'tsc -p tsconfig.json --noEmit',
+    ],
 };
