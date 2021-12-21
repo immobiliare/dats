bfc3a65b7e7641a599248e4070259a4935ce6bb9
github-action-benchmark <github@users.noreply.github.com>
2021-12-21 12:28:38 +0000

    add Benchmark.js Benchmark (benchmarkjs) benchmark result for 069d6fcd22dd5dd38311924412b631a45617ff4b

---
+++ dev/bench/data.js
@@ -0,0 +1,70 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1640089718895,
+  "repoUrl": "https://github.com/immobiliare/dats",
+  "entries": {
+    "Benchmark.js Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "immobiliare",
+            "username": "immobiliare"
+          },
+          "committer": {
+            "name": "immobiliare",
+            "username": "immobiliare"
+          },
+          "id": "069d6fcd22dd5dd38311924412b631a45617ff4b",
+          "message": "chore: added benchmarks",
+          "timestamp": "2021-12-21T09:21:11Z",
+          "url": "https://github.com/immobiliare/dats/pull/93/commits/069d6fcd22dd5dd38311924412b631a45617ff4b"
+        },
+        "date": 1640089717993,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "dats counter base",
+            "value": 359896,
+            "range": "±4.01%",
+            "unit": "ops/sec",
+            "extra": "73 samples"
+          },
+          {
+            "name": "dats tcp",
+            "value": 1861766,
+            "range": "±2.25%",
+            "unit": "ops/sec",
+            "extra": "84 samples"
+          },
+          {
+            "name": "dats buff counter",
+            "value": 1968892,
+            "range": "±3.30%",
+            "unit": "ops/sec",
+            "extra": "78 samples"
+          },
+          {
+            "name": "dats buff gauge",
+            "value": 2040859,
+            "range": "±2.78%",
+            "unit": "ops/sec",
+            "extra": "83 samples"
+          },
+          {
+            "name": "dats buff set",
+            "value": 2068032,
+            "range": "±2.86%",
+            "unit": "ops/sec",
+            "extra": "84 samples"
+          },
+          {
+            "name": "dats buff timing",
+            "value": 1929511,
+            "range": "±3.34%",
+            "unit": "ops/sec",
+            "extra": "76 samples"
+          }
+        ]
+      }
+    ]
+  }
+}
