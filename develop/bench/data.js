77a4a496377954600d4496020b8a030baf09860e
github-action-benchmark <github@users.noreply.github.com>
2022-01-20 13:31:56 +0000

    add Benchmarks (benchmarkjs) benchmark result for 63ab929646b417a4ddd7ac1acb4398a06a940e27

--- develop/bench/data.js
+++ develop/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1642598441378,
+  "lastUpdate": 1642685516857,
   "repoUrl": "https://github.com/immobiliare/dats",
   "entries": {
     "Benchmarks": [
@@ -1518,6 +1518,72 @@ window.BENCHMARK_DATA = {
             "range": "±7.41%",
             "unit": "ops/sec",
             "extra": "79 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "simonecorsi.dev@gmail.com",
+            "name": "Simone Corsi",
+            "username": "simonecorsi"
+          },
+          "distinct": true,
+          "id": "63ab929646b417a4ddd7ac1acb4398a06a940e27",
+          "message": "chore(deps-dev): bump @types/sinon from 10.0.6 to 10.0.8\n\nBumps [@types/sinon](https://github.com/DefinitelyTyped/DefinitelyTyped/tree/HEAD/types/sinon) from 10.0.6 to 10.0.8.\n- [Release notes](https://github.com/DefinitelyTyped/DefinitelyTyped/releases)\n- [Commits](https://github.com/DefinitelyTyped/DefinitelyTyped/commits/HEAD/types/sinon)\n\n---\nupdated-dependencies:\n- dependency-name: \"@types/sinon\"\n  dependency-type: direct:development\n  update-type: version-update:semver-patch\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>",
+          "timestamp": "2022-01-20T14:30:25+01:00",
+          "tree_id": "9ede1cd8a0bf06454c45a54813bec0c856d6d909",
+          "url": "https://github.com/immobiliare/dats/commit/63ab929646b417a4ddd7ac1acb4398a06a940e27"
+        },
+        "date": 1642685516403,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "dats counter udp base",
+            "value": 87502,
+            "range": "±1.68%",
+            "unit": "ops/sec",
+            "extra": "80 samples"
+          },
+          {
+            "name": "dats counter udp buffered",
+            "value": 777222,
+            "range": "±2.15%",
+            "unit": "ops/sec",
+            "extra": "84 samples"
+          },
+          {
+            "name": "dats gauge udp buffered",
+            "value": 803921,
+            "range": "±1.78%",
+            "unit": "ops/sec",
+            "extra": "87 samples"
+          },
+          {
+            "name": "dats set udp buffered",
+            "value": 796129,
+            "range": "±2.08%",
+            "unit": "ops/sec",
+            "extra": "83 samples"
+          },
+          {
+            "name": "dats timing udp buffered",
+            "value": 767712,
+            "range": "±1.69%",
+            "unit": "ops/sec",
+            "extra": "84 samples"
+          },
+          {
+            "name": "dats counter tcp buffered",
+            "value": 1613674,
+            "range": "±7.52%",
+            "unit": "ops/sec",
+            "extra": "64 samples"
           }
         ]
       }
