2fac4aa7bfe7b9363c3c897aa145c62f256c098a
Simone Corsi <simonecorsi.dev@gmail.com>
2024-05-21 10:14:59 +0200

    fix: commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,13 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'body-max-length': [0, 'always'],
+        'body-max-line-length': [0, 'always'],
+        'footer-max-length': [0, 'always'],
+        'footer-max-line-length': [0, 'always'],
+        'header-max-length': [0, 'always'],
+        'scope-max-length': [0, 'always'],
+        'subject-max-length': [0, 'always'],
+        'type-max-length': [0, 'always'],
+    },
+};
